6f3104aaef092caef5433f58f2497db4a5a2bc6b
omar <omarmax546@gmail.com>
2025-08-30 23:12:13 -0500

    Se corrigio y reestructuro la logica principal del programa

--- Proyecto Mototech.py
+++ Proyecto Mototech.py
@@ -49,7 +49,7 @@ def listar_modelos():
 
 
 def buscar_moto_por_modelo(modelo_a_buscar):
-#busca y devuelva los datos de una moto po su modelo
+#busca y devuelva los datos de una moto por su modelo
     for moto in base_de_datos_de_motos:
         if moto["modelo"].strip()==modelo_a_buscar.strip():
             return moto
@@ -98,50 +98,49 @@ if opcion == "1":
 
     if moto_encontrada:
      #1. creamos y guardamos el cliente
-         nombre_cliente = input("por  favor ingresa tu nombre:")
-         telefono_cliente = input("por favor ingresa tu numero de telefono:")
-         crear_cliente (nombre_cliente, telefono_cliente, moto_encontrada)
+        nombre_cliente = input("por  favor ingresa tu nombre:")
+        telefono_cliente = input("por favor ingresa tu numero de telefono:")
+        crear_cliente (nombre_cliente, telefono_cliente, moto_encontrada)
 
 
     #2. revisamos el kilometraje
-    try:
-        solictud_kilometraje_actual  = int(input("ingresa el kilometraje actual de la moto:"))
-        if solictud_kilometraje_actual >= moto_encontrada["intervalo"]:
-            print(f"\n¡Alerta! es hora de realizar el mantenimiento de la {moto_encontrada["modelo"]}")
-            print(f"\nlas tareas reomendadas son: {moto_encontrada["mantenimiento"]}")
-            print(f"recuerda aplicar el aceite recomendado para este modelo de moto, el: {moto_encontrada["tipo_aceite"]}" )
-        else:
-            kilometraje_restante = moto_encontrada["intervalo"] - solictud_kilometraje_actual
-            print(f"\nAun no es necesario realizar al mantenimiento")
-            print(f"\nfaltan {kilometraje_restante} para el proximo servicio")
+        try:
+            solictud_kilometraje_actual  = int(input("ingresa el kilometraje actual de la moto:"))
+            if solictud_kilometraje_actual >= moto_encontrada["intervalo"]:
+                print(f"\n¡Alerta! es hora de realizar el mantenimiento de la {moto_encontrada["modelo"]}")
+                print(f"\nlas tareas reomendadas son: {moto_encontrada["mantenimiento"]}")
+                print(f"recuerda aplicar el aceite recomendado para este modelo de moto, el: {moto_encontrada["tipo_aceite"]}" )
+            else:
+                kilometraje_restante = moto_encontrada["intervalo"] - solictud_kilometraje_actual
+                print(f"\nAun no es necesario realizar al mantenimiento")
+                print(f"\nfaltan {kilometraje_restante} para el proximo servicio")
 
-        solicitud_costo_repuesto = float(input("\npor favor ingresa el costo del repuesto:"))
-        solicitud_costo_mano_de_obra = float(input("por favor ingresa el costo de la mano de obra:"))
-        costo_total_final = calcular_costo_servicio (solicitud_costo_repuesto,solicitud_costo_mano_de_obra )
-        print(f"el costo total de servicio es: {costo_total_final}")
+            solicitud_costo_repuesto = float(input("\npor favor ingresa el costo del repuesto:"))
+            solicitud_costo_mano_de_obra = float(input("por favor ingresa el costo de la mano de obra:"))
+            costo_total_final = calcular_costo_servicio (solicitud_costo_repuesto,solicitud_costo_mano_de_obra )
+            print(f"el costo total de servicio es: {costo_total_final}")
 
 
     #crea un diccionario con los datos del servicio actual
-        nuevo_servicio = {
-            "fecha" : "21/08/2025",  #usaremos una fecha fija por ahora
-            "kilometraje" : solictud_kilometraje_actual,
-            "costo" : costo_total_final,
-            "tareas realizadas" : moto_encontrada ["mantenimiento"]
-    }
+            nuevo_servicio = {
+                "fecha" : "21/08/2025",  #usaremos una fecha fija por ahora
+                "kilometraje" : solictud_kilometraje_actual,
+                "costo" : costo_total_final,
+                "tareas realizadas" : moto_encontrada ["mantenimiento"]
+            }
 
 #agregar el servicio al historial del cliente
         
-        base_de_datos_de_clientes [nombre_cliente] ["historial_mantenimientos"].append (nuevo_servicio)
-        print("\nEl servicio ha sido guardado en el historial de mantenimientos del cliente.")
+            base_de_datos_de_clientes [nombre_cliente] ["historial_mantenimientos"].append (nuevo_servicio)
+            print("\nEl servicio ha sido guardado en el historial de mantenimientos del cliente.")
 
-    except ValueError: 
-        print(f"\nEntrada invalida, por favor, ingresa solo numeros para los costos")
+        except ValueError: 
+            print(f"\nEntrada invalida, por favor, ingresa solo numeros para los costos")
        
     else:
         print(f"lo siento, el modelo '{modelo_buscado}' no se encontró ") 
 
     
-
 
 elif opcion == "2":
     nombre_cliente = input ("Por favor,  ingresa el nombre del cliente: ")
