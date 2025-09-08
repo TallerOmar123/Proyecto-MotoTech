cfe9136163c6bd6b6214d33a8dc43ec9bdf8c5f5
omar <omarmax546@gmail.com>
2025-09-07 23:58:21 -0500

    Se completó la funcion de busqueda y se probó la lógica de if/else.

--- pyto(gestion de mantenimiento de motos).py
+++ pyto(gestion de mantenimiento de motos).py
@@ -43,17 +43,43 @@ base_de_datos_de_motos= [
     }
 ]
 
-# agrego un diccionario vacion en este software para basicamente guardar en 
+# agrego un diccionario vacio en este software para basicamente guardar en 
 # el toda la informacion del usuario que se me entegara en el input de los 
 #datos para poder ejecutar el programa
 base_de_datos_de_cliente = {}
 
 #Ahora la tarea es definir una funcion que me muestre los modelos de motos 
 # disponibles que tengo en este programa.
-#nota: los parentesis vacios indican que no necesoto ningun argumento
+#nota: los parentesis vacios indican que no necesito ningun argumento
 def listar_motos ():
     print("Los diferentes modelos de motos disponibles son:")
     for moto in base_de_datos_de_motos:
         print(f" - {moto['modelo']}")
 
+
+
+#ahora creo otra funcion que me permita recorrer la moto en base de datos
+#con el input que me entregue el cliente
+def buscar_moto_por_modelo(modelo_a_buscar):
+    for moto_encontrada in base_de_datos_de_motos:
+        if moto_encontrada['modelo'] == modelo_a_buscar:
+            return moto_encontrada
+    return None 
+
+
+
+
+
+#aqui empiezo a dar logica al programa
 listar_motos()
+#en la llamda a la funcion "buscar moto por modelo" primero coloco un input
+modelo_buscado = input("Por favor ingresa el modelo de tu moto")
+#despues llamo a la funcion
+buscar_moto_por_modelo(modelo_buscado)
+#luego creo otra variable en el que guardo en retorno de la operacion cuando llamo a la funcion
+moto_ya_econtrada = buscar_moto_por_modelo (modelo_buscado)
+if moto_ya_econtrada:
+    print("hemos encontrado tu  modelo de moto")
+else:
+    print("lo siento, no hemos podido encontrar tu modelo de moto")
+
