00c355d5466c5c2ad7e361b407b6e9850779eac5
omar <omarmax546@gmail.com>
2025-09-10 22:28:03 -0500

    Se agregó la funcionalidad para ver el historial de mantenimientos en el menú principal.

--- pyto(gestion de mantenimiento de motos).py
+++ pyto(gestion de mantenimiento de motos).py
@@ -84,6 +84,9 @@ def registrar_cliente():
     }
     base_de_datos_de_cliente[solicitud_de_datos_nombre] = nuevo_cliente
     print("Gracias por la informacion, tus datos han sido registrados")
+#Nota: la diferencia de esta solicitud de nombre en este caso es el registro del cliente. pues aqui estoy guardando en base de datos
+#de cliente. bajo la etiqueta nuevo cliente. pero en registrar mantenimiento utilizo el nombre que ya ha sido registrado en registrar
+# cliente, esa es la diferencia de los dos, basicamente uno se liga con el otro
 
 def registrar_mantenimiento():
     solicitud_de_datos = input("por favor escribe aqui tu nombre:").strip() .lower()
@@ -91,18 +94,39 @@ def registrar_mantenimiento():
         solicitud_kilometraje_actual = input("Por favor escribe el kilometraje de tu moto")
         solicitud_costo_servicio = input("Por favor, escribe el costo del servicio")
         solicitud_descripcion_trabajo_realizado = input("coloca aqui el trabajo que se le ha realizado a tu moto")
-        nuevo_manetenimiento = {
+        nuevo_mantenimiento = {
             "kilometraje" : solicitud_kilometraje_actual,
             "costo" : solicitud_costo_servicio,
             "descipcion" : solicitud_descripcion_trabajo_realizado
         }
-        cliente_encontrado = base_de_datos_de_cliente[solicitud_de_datos]
-        cliente_encontrado ['historial_mantenimientos'].append (nuevo_manetenimiento)
+        cliente_encontrado = base_de_datos_de_cliente[solicitud_de_datos] #esta primera linea tiene una variable que guarda en base
+#de datos de cliente, garda todo el diccionario que relaciona registrar mantenimiento.
+        cliente_encontrado ['historial_mantenimientos'].append (nuevo_mantenimiento) #aqui es diferente pues no hay una variable que 
+# guarda en base de datos, es una modificacion con append en el deccionario que habia dejado abierto en nuevo cliente... historial
+#mantenimientos
         print("!felicidades, has sido encontrado!")
     else:
         print("lo siento, no hemos encontrado tu usuario")
 
-    
+def ver_historial_mantenimientos():
+    solicitud_nombre_cliente = input("Pr favor, escribe tu nombre para poder buscar la informacion de tu moto")
+    if solicitud_nombre_cliente in base_de_datos_de_cliente:
+        cliente_encontrado = base_de_datos_de_cliente[solicitud_nombre_cliente]
+        historial = cliente_encontrado['historial_mantenimientos']
+        if historial: # Verificamos si la lista no está vacía
+            print(f"\nHistorial de mantenimientos para {solicitud_nombre_cliente.title()}:")
+            for servicio in historial:
+                print("---")
+                for clave, valor in servicio.items():
+                    print(f"  {clave.title()}: {valor}")
+            print("---")
+        else:
+            print("Este cliente no tiene mantenimientos registrados.") 
+        
+        
+    else:
+        print("Lo siento, no encontramos este usuario")
+
 
 
 
@@ -115,13 +139,15 @@ while True:
     print("2. buscar una moto por modelo")
     print("3. registrar un nuevo cliente")
     print("4. salir")
-    opcion = input("Por favor selecciona una opcion:").strip()
+    opcion = input("Por favor selecciona una opcion:").lower().strip()
 
     if opcion == "1":
         listar_motos() ##aqui empiezo a dar logica al programa
     elif opcion == "2":
-        modelo_buscado = input("Por favor ingresa el modelo de tu moto:").strip() .lower() #aqui hago una llamada a la funcion de buscar modelo con un input
-        moto_ya_econtrada = buscar_moto_por_modelo(modelo_buscado)   #luego creo otra variable en el que guardo en retorno de la operacion cuando llamo a la funcion
+        modelo_buscado = input("Por favor ingresa el modelo de tu moto:").strip() .lower() #aqui hago una llamada a 
+#la funcion de buscar modelo con un input
+        moto_ya_econtrada = buscar_moto_por_modelo(modelo_buscado)   #luego creo otra variable en el que guardo en retorno de 
+#la operacion cuando llamo a la funcion
         if moto_ya_econtrada:
             print("!Moto encontrada¡ aqui estan los detalles")
             for clave, valor in moto_ya_econtrada.items():
